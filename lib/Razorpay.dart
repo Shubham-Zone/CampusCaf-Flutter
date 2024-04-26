f31804bd1e697a0432c9aeabe5d61fd336c07808
Shubham-Zone <154971588+Shubham-Zone@users.noreply.github.com>
2024-04-26 12:49:04 +0530

    Update Razorpay.dart

--- lib/Razorpay.dart
+++ lib/Razorpay.dart
@@ -92,39 +92,6 @@ class _RazorpaypaymentState extends State<Razorpaypayment> {
     }
   }
 
-  // void sendPushMessage(String token, String body, String title) async {
-  //   try {
-  //     await http.post(
-  //       Uri.parse('https://fcm.googleapis.com/fcm/send'),
-  //       headers: <String, String>{
-  //         'Content-type': 'application/json',
-  //         'Authorization':
-  //         'key=AAAAuUXiJEQ:APA91bHhIMMpIefv42kJteKdqWXg4Sfku64ZzrnhEdw826n8oLIhx62Db7F8OU8PqIC8lqyz4akIrgooaGjX8lr4tXbREsLmmhcs7DNKun1mTCY7EpfJKvv3Ksdi3qjIyi2ci1zvQQVc'
-  //       },
-  //       body: jsonEncode(
-  //         <String, dynamic>{
-  //           'priority': 'high',
-  //           'data': <String, dynamic>{
-  //             'click-action': 'FLUTTER_NOTIFICATION_CLICK',
-  //             'Status': 'done',
-  //             'body': body,
-  //             'title': title,
-  //           },
-  //           "notification": <String, dynamic>{
-  //             "title": title,
-  //             "body": body,
-  //             "android_channel_id": "Cafeteria"
-  //           },
-  //           "to": token,
-  //         },
-  //       ),
-  //     );
-  //   } catch (e) {
-  //     if (kDebugMode) {
-  //       print("error push notification");
-  //     }
-  //   }
-  // }
 
   void sendPushMessage(String body,String title) async{
     try{
@@ -132,8 +99,7 @@ class _RazorpaypaymentState extends State<Razorpaypayment> {
         Uri.parse('https://fcm.googleapis.com/fcm/send'),
         headers:<String,String>{
           'Content-type': 'application/json',
-          'Authorization': 'key=AAAAuUXiJEQ:APA91bHhIMMpIefv42kJteKdqWXg4Sfku64ZzrnhEdw826n8oLIhx62Db7F8OU8PqIC8lqyz4akIrgooaGjX8lr4tXbREsLmmhcs7DNKun1mTCY7EpfJKvv3Ksdi3qjIyi2ci1zvQQVc'
-
+          'Authorization': 'KEY'
         },
         body:jsonEncode(
           <String,dynamic>{
